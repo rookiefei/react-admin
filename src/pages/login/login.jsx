826bb0e8f33f515c50940cb76b79817928980e63
zf <592436263@qq.com>
2020-11-15 22:30:41 +0800

    登录

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -8,14 +8,45 @@ import {
   Input,
   Button
 } from 'antd';
+import {reqLogin} from '../../api'
+
 
 class Login extends Component {
   handleSubmit = (event) => {
+    console.log(event)
     event.preventDefault()
+    this.props.form.validateFields(async(err, values) => {
+      if (!err) {
+        const {username, password} = values
+        try {
+          const response = await reqLogin(username, password)
+        } catch (error) {
+          console.log('请求出错！')
+        }
+        
+        console.log(response)
+      }
+    })
     const form = this.props.form
     const values = form.getFieldsValue()
     console.log(values)
   }
+  /*
+  对密码进行自定义验证
+  */
+ validatePwd = (rule, value, callback) => {
+   if (!value) {
+     callback('密码必须输入！')
+   } else if (value.length<4) {
+     callback('密码长度不能小于4！')
+   } else if (value.length>12) {
+     callback('密码长度不能大于12位')
+   } else if (!/^[a-zA-Z0-9_]+$/.test(value)) {
+     callback('密码必须是英文、数字或下划线组成！')
+   } else {
+     callback()
+   }
+ }
   render() {
     const { getFieldDecorator } = this.props.form;
     return (
@@ -29,7 +60,13 @@ class Login extends Component {
           <Form className="login-form" onSubmit={this.handleSubmit}>
             <Form.Item>
               {getFieldDecorator('username', {
-                rules: [{ required: true, message: 'Please input your username!' }],
+                // 声明式验证：直接使用别人定义好的验证规则进行验证
+                rules: [
+                  { required: true, whitespace: true, message: 'Please input your username!' },
+                  { min: 2, message: "最少输入2个字符"},
+                  { max: 12, message: "至多输入12个字符"},
+                  { pattern: /^[a-zA-Z0-9_]+$/, message: "用户名必须是英文、数字或下划线组成！"}
+                ],
               })(
                 <Input
                   prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
@@ -39,7 +76,7 @@ class Login extends Component {
             </Form.Item>
             <Form.Item>
               {getFieldDecorator('password', {
-                rules: [{ required: true, message: 'Please input your username!' }],
+                rules: [{ validator: this.validatePwd }],
               })(
                 <Input
                   prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
